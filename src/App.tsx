bc0499f580b4845bc8bed51ed9d57fc06daf4e9d
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-09-02 17:43:26 +0600

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 // src/App.tsx
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { useRouter } from "./hooks/useRoutes";
 import ScrollButton from "./components/WebsiteReview/ScrollButton";
+import Loader from "./components/Loader/loader";
 
 type TAppProps = object;
 
 const App: FC<TAppProps> = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoading = () => setLoading(false);
+
+    // Listen for the router to finish loading
+    window.addEventListener("load", handleLoading);
+
+    return () => {
+      window.removeEventListener("load", handleLoading);
+    };
+  }, []);
 
   return (
     <main>
+      {!loading && <Loader />}
       <RouterProvider router={router} />
       <ScrollButton />
     </main>
